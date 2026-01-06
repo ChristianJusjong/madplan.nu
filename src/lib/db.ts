ec1cafdb3bf91bfa6b630c57a898706644f03b83
Christian Jusjong <christian.jusjong@ganni.com>
2026-01-06 11:15:13 +0100

    Fix Prisma Client initialization using @prisma/adapter-pg

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,12 +1,18 @@
+import { Pool } from "pg";
+import { PrismaPg } from "@prisma/adapter-pg";
 import { PrismaClient } from "@prisma/client";
 
 declare global {
   var prisma: PrismaClient | undefined;
 }
 
-// Explicitly pass connection URL to support Prisma 7 / Schema-less config
+const connectionString = `${process.env.DATABASE_URL}`;
+
+const pool = new Pool({ connectionString });
+const adapter = new PrismaPg(pool);
+
 export const db =
   globalThis.prisma ||
-  new PrismaClient();
+  new PrismaClient({ adapter });
 
 if (process.env.NODE_ENV !== "production") globalThis.prisma = db;
